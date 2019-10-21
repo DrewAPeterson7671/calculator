4f3d09d29de277d75ea11073a28a5d67232ebf74
Drew Peterson and Hristo Dzhambov <drew@>
2019-10-21 16:39:12 -0700

    add Javascript add, subtract, multiply, and divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 var add = function(number1, number2) {
-  return number1 + number2;
+  alert(number1 + number2);
 };
 
 var subtract = function(number1, number2) {
-  return number1 - number2;
+  alert(number1 - number2);
 };
 
 var multiply = function(number1, number2) {
-  return number1 * number2;
+  alert(number1 * number2);
 };
 
 var divide = function(number1, number2) {
-  return number1 / number2;
+  alert(number1 / number2);
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(divide(number1, number2));
+add(number1, number2);
+
+//var bmi = function(number1, number2) {
+//  return 703 * (number1 / (number2 * number2));
+//};
+
+
+//var toFahr = function(number1) {
+//  alert(32 + (1.8 * number1));
+//};
